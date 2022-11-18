5c3592ee23aad255629c6f09daf080dd1e97a2a4
caternuson <caternuson@gmail.com>
2022-11-18 11:41:07 -0800

    fix example, remove AVR trinkets from CI

--- examples/Autoscroll/Autoscroll.ino
+++ examples/Autoscroll/Autoscroll.ino
@@ -1,14 +1,14 @@
 /*
   LiquidCrystal Library - Autoscroll
- 
+
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
+ library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
- 
+
  This sketch demonstrates the use of the autoscroll()
  and noAutoscroll() functions to make new text scroll or not.
- 
+
  The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -19,28 +19,28 @@
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
- 
+
  Library originally added 18 Apr 2008
  by David A. Mellis
  library modified 5 Jul 2009
  by Limor Fried (http://www.ladyada.net)
  example added 9 Jul 2009
- by Tom Igoe 
+ by Tom Igoe
  modified 25 July 2009
  by David A. Mellis
- 
+
  http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
 // include the library code:
 #include "Wire.h"
-#include "LiquidCrystal.h"
+#include "Adafruit_LiquidCrystal.h"
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+Adafruit_LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows:
   lcd.begin(16,2);
 }
 
@@ -64,7 +64,7 @@ void loop() {
   }
   // turn off automatic scrolling
   lcd.noAutoscroll();
-  
+
   // clear screen for the next loop:
   lcd.clear();
 }

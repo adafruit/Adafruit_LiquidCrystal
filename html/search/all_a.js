d2d1d3a59fc41ac5a0d1f34968a91f182cd7a263
Doxygen CI <ci-arduino@invalid>
2022-11-28 22:41:06 +0000

    Deploy docs to GitHub Pages from commit 3fb2d078b3
    Commit: 3fb2d078b3e1366417a1f09763de4cf0724bcc2a
    GitHub Actions run: 3569187599

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['scrolldisplayleft',['scrollDisplayLeft',['../class_adafruit___liquid_crystal.html#ae59dd79aae724ef80dc37caddc240600',1,'Adafruit_LiquidCrystal']]],
   ['scrolldisplayright',['scrollDisplayRight',['../class_adafruit___liquid_crystal.html#a6ec792056b816dc20376c0116f1f743e',1,'Adafruit_LiquidCrystal']]],
-  ['setbacklight',['setBacklight',['../class_adafruit___liquid_crystal.html#a23175fd7037d2b1c4ca046305d3418b7',1,'Adafruit_LiquidCrystal']]],
+  ['setbacklight',['setBacklight',['../class_adafruit___liquid_crystal.html#a2205e61c9f4f0e5da8b8465dfa11c734',1,'Adafruit_LiquidCrystal']]],
   ['setcursor',['setCursor',['../class_adafruit___liquid_crystal.html#a6f70b0443d87b92a4804ff907e463eaf',1,'Adafruit_LiquidCrystal']]]
 ];
